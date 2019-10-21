6eb4d1fd08205275a6209499095e66d6b72070fe
Reginaldo Junior <juniorjp1989@gmail.com>
2019-10-21 02:43:14 -0300

    Add parking create method

--- app/controllers/v1/parkings_controller.rb
+++ app/controllers/v1/parkings_controller.rb
@@ -1,6 +1,17 @@
 module V1
   class ParkingsController < ApplicationController
 
+    #TODO: do not create if there is an existing parking with same plate number with in_progress status
+    def create
+      success, @parking = Parking.create_default(params[:plate])
+
+      if success
+        render :parking
+      else
+        render json: {error: @parking.errors.details}, status: 422
+      end
+    end
+
     def show
       @parkings = Parking.where(plate: params[:plate]).order(id: :desc)
       render :parkings
--- spec/controllers/v1/parkings_controller_spec.rb
+++ spec/controllers/v1/parkings_controller_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe V1::ParkingsController, type: :controller do
+  
+  describe 'POST #create' do
+    render_views
+    let(:plate_number) { 'GTX-4498' }
+    let(:invalid_plate_number){ 'IUU-776555' }
+
+    it 'returns http success with a new parking model' do
+      @request.headers['api_version'] = 'v1'
+      post :create, params: {plate: plate_number}, format: :json
+      expect(response).to have_http_status(200)
+      expect(Parking.count).to eq(1)
+      parking = Parking.last
+      expect(parking.plate).to eq(plate_number)
+    end
+
+    it 'returns 422 if the plate number is invalid' do
+      post :create, params: {plate: invalid_plate_number}, format: :json
+      expect(response).to have_http_status(422)
+      expect(Parking.count).to eq(0)
+    end
+  end
 
   describe 'GET #show' do
     render_views
