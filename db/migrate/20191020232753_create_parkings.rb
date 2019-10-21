731aedadd19cf370a5ff02fe3abbb378bcc28566
Reginaldo Junior <juniorjp1989@gmail.com>
2019-10-21 00:08:02 -0300

    Add missing parking paid field

--- db/migrate/20191020232753_create_parkings.rb
+++ db/migrate/20191020232753_create_parkings.rb
@@ -3,6 +3,7 @@ class CreateParkings < ActiveRecord::Migration[6.0]
     create_table :parkings do |t|
       t.string :plate, index: true
       t.integer :status, limit: 2
+      t.boolean :paid, default: false
       t.datetime :finished_at
       t.timestamps
     end
