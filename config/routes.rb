33a499e5fee591e783cc67171fa4a8b4ee9aedeb
Reginaldo Junior <juniorjp1989@gmail.com>
2019-10-21 01:36:36 -0300

    Config route endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #Check the header for api_version key
+  api_version(module: 'V1', header: { name: 'api_version', value: 'v1' }, default: true) do
+
+    #nested resources
+    resources :parkings, defaults: { format: 'json' }, only: [] do
+      member do
+        resource :pay, only: [:update], controller: 'parkings/pay'
+        resource :out, only: [:update], controller: 'parkings/out'
+      end
+    end
+
+    #main resources
+    resources :parkings, defaults: { format: 'json' }, only: [:show], param: :plate
+  end
 end
